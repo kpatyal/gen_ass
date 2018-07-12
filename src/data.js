4d325d69d29412848edda78c2c9594da4daebb29
kpatyal <ptlkaran499@gmail.com>
2018-07-12 13:28:27 +0530

    update data format for locations.

--- src/data.js
+++ src/data.js
@@ -1,195 +1,29 @@
-/*export const countries = [
-	{
-		"name": "India",
-		"key": "IN"
-	},
-	{
-		"name": "United Kingdom",
-		"key": "UK"
-	},
-	{
-		"name": "United State",
-		"key": "US"
-	}
-];*/
-
 export const countries = ["India", "United Kingdom", "United State"];
-
-
-
 export const states = {
-	"IN": ["Karnataka","Delhi"],
-	"UK": ["England", "Northern Ireland"],
-	"US": ["New York", "Texas"]
+	"India": ["Karnataka","Delhi"],
+	"United Kingdom": ["England", "Northern Ireland"],
+	"United State": ["New York", "Texas"]
 	
 }
-
 export const area = {
-	"BLR": [
-		{
-			"name": "BTM",
-			"key": "BTM"
-		},
-		{
-			"name": "HSR",
-			"key": "HSR"
-		}
-	],
+	"Bangalore": ["BTM", "HSR"],
+	"Hubbli": ["Hubbli1", "Hubbli2"],
 
-	"HB": [
-		{
-			"name": "Hubbli1",
-			"key": "HB1"
-		},
-		{
-			"name": "Hubbli2",
-			"key": "HB2"
-		}
-	],
-
-	"NOR": [
-		{
-			"name": "South Bank",
-			"key": "SB"
-		},
-		{
-			"name": "Notting Hill",
-			"key": "NH"
-		}
-			
-	],
-	"LDN": [
-		{
-			"name": "South Bank",
-			"key": "SB"
-		},
-		{
-			"name": "Notting Hill",
-			"key": "NH"
-		}
-	],
-	"BEL": [
-		{
-			"name": "BTM",
-			"key": "BTM"
-		},
-		{
-			"name": "HSR",
-			"key": "HSR"
-		}
-	],
-	"DER": [
-		{
-			"name": "BTM",
-			"key": "BTM"
-		},
-		{
-			"name": "HSR",
-			"key": "HSR"
-		}
-	],
-	"NYC": [
-		{
-			"name": "BTM",
-			"key": "BTM"
-		},
-		{
-			"name": "HSR",
-			"key": "HSR"
-		}
-	],
-	"ELM": [
-		{
-			"name": "BTM",
-			"key": "BTM"
-		},
-		{
-			"name": "HSR",
-			"key": "HSR"
-		}
-	],
-	"HOU": [
-		{
-			"name": "BTM",
-			"key": "BTM"
-		},
-		{
-			"name": "HSR",
-			"key": "HSR"
-		}
-	],
-	"AUS": [
-		{
-			"name": "BTM",
-			"key": "BTM"
-		},
-		{
-			"name": "HSR",
-			"key": "HSR"
-		}
-	],
+	"Norfolk": ["South Bank", "Notting Hill"],
+	"London": ["South Bank", "Notting Hill"],
+	"Belfast":  ["BTM", "HSR"],
+	"Derry":  ["BTM", "HSR"],
+	"New York City":  ["BTM", "HSR"],
+	"Elmira":  ["BTM", "HSR"],
+	"Houston": ["BTM", "HSR"],
+	"Austin":  ["BTM", "HSR"],
 	
 }
-
 export const cities = {
-	"KA": [
-		{
-			"name": "Bangalore",
-			"key": "BLR"
-		},
-		{
-			"name": "Hubbli",
-			"key": "HB"
-		}
-	],
-	"DL": [
-		{
-			"name": "Bangalore",
-			"key": "BLR"
-		},
-		{
-			"name": "Hubbli",
-			"key": "HB"
-		}
-	],
-	"ENG": [
-		{
-			"name": "Norfolk",
-	        "key": 'NOR'
-		},
-		{
-			"name": "London",
-	        "key": 'LDN'
-		}
-	],
-	"NIR": [
-        {
-			"name": "Belfast",
-	        "key": 'BEL'
-		},
-		{
-			"name": "Derry",
-	        "key": 'DER'
-		}
-	],
-	"NY": [
-        {
-			"name": "New York City",
-	        "key": 'NYC'
-		},
-		{
-			"name": "Elmira",
-	        "key": 'ELM'
-		}
-	],
-	"TX": [
-        {
-			"name": "Houston",
-	        "key": 'HOU'
-		},
-		{
-			"name": "Austin",
-	        "key": 'AUS'
-		}  
-	]
+	"Karnataka": ["Bangalore", "Hubbli"],
+	"Delhi": ["Bangalore", "Hubbli"],
+	"England": ["Norfolk", "London"],
+	"Northern Ireland": ["Belfast",  "Derry"],
+	"New York": ["New York City", "Elmira"],
+	"Texas": ["Houston", "Austin"]
 }
